e3912c3b84c9aa41dfe86bdb6f181b1a88744429
Pau <pmendezsantochi@gmail.com>
2024-09-11 12:32:09 -0300

    agregue funciones a algunos componentes

--- src/components/GalleryApp.jsx
+++ src/components/GalleryApp.jsx
@@ -1,22 +1,37 @@
-import React from 'react'
-import { useState } from 'react';
+import React from "react";
+import { useState } from "react";
 
 const GalleryApp = (props) => {
-   const[indice,setIndice]= useState(0)
-    const {esculturas} = props
+  const [indice, setIndice] = useState(0);
+  const { esculturas } = props;
+
+  const [show, setShow] = useState(false);
+
+  const handleNext = () => {
+    if (indice < esculturas.length - 1) setIndice(indice + 1);
+  };
+  const handlePrev = () => {
+    if (indice > 0) {
+      setIndice(indice + 1);
+    }
+  };
   return (
     <article>
-        <button>Siguiente</button>
-        <section>
-            <h3> {esculturas[indice].name}</h3>
-            <small>{esculturas[indice].artist}</small>
-        </section>
-        <section>
-            <img src={esculturas[indice].url} alt={esculturas[indice].alt} />
-        </section>
-        <section><p>{esculturas[indice].description}</p></section>
+      <button onClick={handlePrev}>Anterior</button>
+      <button onClick={handleNext}>Siguiente</button>
+      <section>
+        <h3> {esculturas[indice].name}</h3>
+        <small>{esculturas[indice].artist}</small>
+      </section>
+      <section>
+        <img src={esculturas[indice].url} alt={esculturas[indice].alt} />
+      </section>
+      <section>
+        <button onClick={()=> setShow(!show)}>Descripcion</button>
+        {show && <p>{esculturas[indice].description}</p>}
+      </section>
     </article>
-  )
-}
+  );
+};
 
-export default GalleryApp
+export default GalleryApp;
